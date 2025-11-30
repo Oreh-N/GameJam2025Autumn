7c8c1b6053b2002aac47cbd0c7eecc3269a56b74
Anastasiia Orekhovych <nastia.orehowich@gmail.com>
2025-11-30 12:17:38 +0100

    Add prefabs
    Add UI
    Add prefabs on scene

---
+++ Hunger or Anger/Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Item: MonoBehaviour
+{
+	public Sprite item;
+	public Sprite brokenItem;
+
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		
+	}
+}
