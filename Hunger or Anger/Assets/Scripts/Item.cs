7ef041525b00d27f60ad851c4a748e32b235c577
Anastasiia Orekhovych <nastia.orehowich@gmail.com>
2025-11-30 13:42:41 +0100

    Add game interaction

--- Hunger or Anger/Assets/Scripts/Item.cs
+++ Hunger or Anger/Assets/Scripts/Item.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class Item: MonoBehaviour
 {
@@ -15,8 +18,33 @@ public class Item: MonoBehaviour
 
 
 
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{ UI.Instance.ShowInteractionButton(false); }
+	}
+
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{ 
+			UI.Instance.ShowInteractionButton(true);
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				Debug.Log("Pressed E");
+				BreakItem();
+			}
+		}
 		
+	}
+
+	private void BreakItem()
+	{
+		GetComponent<SpriteRenderer>().sprite = brokenItem;
+		Destroy(GetComponent<Collider2D>());
 	}
 }
--- Hunger or Anger/Assets/Scripts/UI.cs
+++ Hunger or Anger/Assets/Scripts/UI.cs
@@ -2,16 +2,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class UI : MonoBehaviour
 {
+	public static UI Instance;
 	[SerializeField] Slider attentionSlider;
 	[SerializeField] Slider angerSlider;
 	[SerializeField] Slider hungerSlider;
+	[SerializeField] GameObject interactionButton;
 
+
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		interactionButton.SetActive(false);
+	}
 
 	public void AddValueToSlider(Slider s, int val)
 	{
 		s.value += val;
+	}
+
+	public void ShowInteractionButton(bool show)
+	{
+		interactionButton.SetActive(show);
 	}
 
 	public void StartGame()
